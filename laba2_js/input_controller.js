fad8489d39091239fe84b50636c1eb1104c50812
DaniinXorchenabo <rkbcu@mail.ru>
2021-09-15 22:15:15 +0300

    fixed bugs with last project

--- laba2_js/input_controller.js
+++ laba2_js/input_controller.js
@@ -1,9 +1,12 @@
+const bad_val = "Недопустивое значение";
+const unreal_calc = "Невозможно вычислить";
+
 function corrected_number(input_id){
     "use strict"; // для браузеров с поддержкой строгого режима
 
-    var val = document.getElementById(input_id).value.toString();
-    var testing_num = new RegExp("^([-]?[0-9]{1,}([.][0-9]{1,})?|" + bad_val + "|" + unreal_calc + ")$");
-    var edit_num = new RegExp("^([-]?[0-9]{1,}([.][0-9]{1,})" + bad_val + "|" + unreal_calc + ")?");
+    const val = document.getElementById(input_id).value.toString();
+    const testing_num = new RegExp("^([-]?[0-9]{1,}([.][0-9]{1,})?|" + bad_val + "|" + unreal_calc + ")$");
+    const edit_num = new RegExp("^([-]?[0-9]{1,}([.][0-9]{1,})" + bad_val + "|" + unreal_calc + ")?");
     if (!testing_num.test(val)){
         if (edit_num.test(val)){
             document.getElementById(input_id).value = edit_num.exec(val)[0];
@@ -17,9 +20,9 @@ function corrected_number(input_id){
 function correcting_number(input_id){
     "use strict"; // для браузеров с поддержкой строгого режима
 
-    var val = document.getElementById(input_id).value.toString();
-    var testing_num = new RegExp("^([-]?[0-9]{1,}([.][0-9]*)?|[-]?[0-9]{1,}[.]?|[-]?)$");
-    var edit_num = new RegExp("^([-]?[0-9]{1,}([.][0-9]*)?|[-]?[0-9]{1,}[.]?|[-]?)");
+    const val = document.getElementById(input_id).value.toString();
+    const testing_num = new RegExp("^([-]?[0-9]+([.][0-9]*)?|[-]?[0-9]+[.]?|[-]?)$");
+    const edit_num = new RegExp("^([-]?[0-9]+([.][0-9]*)?|[-]?[0-9]+[.]?|[-]?)");
     if (!testing_num.test(val)){
         if (edit_num.test(val)){
             document.getElementById(input_id).value = edit_num.exec(val)[0];
--- laba2_js/worker.js
+++ laba2_js/worker.js
@@ -1,6 +1,5 @@
-const arr = Array.apply(null, new Array(10e0)).map(ind=>Math.round(Math.random()*10e6))
+const arr = Array.apply(null, new Array(10e0)).map(()=>Math.round(Math.random()*10e6))
 arr.sort()
 const processing = id_element => {
     const key = document.getElementById(id_element).value
-
 }
