555ad8cc99e536fb57a8ca66af769adef0fd2a14
DaniinXorchenabo <rkbcu@mail.ru>
2021-09-16 00:46:35 +0300

    лаба готова

--- laba2_js/worker.js
+++ laba2_js/worker.js
@@ -1,5 +1,151 @@
-const arr = Array.apply(null, new Array(10e0)).map(()=>Math.round(Math.random()*10e6))
-arr.sort()
+let arr = [];
+setTimeout(() => {
+    let i = 0;
+    while (i < 10e5){
+        arr.push(Math.random().toString().substr(3, 5));
+        i++;
+    }
+    arr.sort()
+    arr = arr.map(i => parseInt(i));
+    console.log(arr);
+})
+const count_iter = 10000;
+
 const processing = id_element => {
-    const key = document.getElementById(id_element).value
+    const key = Number(document.getElementById(id_element).value);
+
+    console.time('time_1');
+    let [index, time] = consistent_search(key);
+    console.timeEnd('time_1');
+    time = Math.round(time);
+
+
+    document.getElementById("time_1").textContent = `${time} ms`;
+    document.getElementById("index_1").textContent = index.toString();
+
+    console.time('time_2');
+    [index, time] = binary_search(key);
+    console.timeEnd('time_2');
+    time = Math.round(time);
+
+
+    document.getElementById("time_2").textContent = `${time} ms`;
+    document.getElementById("index_2").textContent = index.toString();
+
+    console.time('time_3');
+    [index, time] = opt_binary_search(key);
+    console.timeEnd('time_3');
+    time = Math.round(time);
+
+    document.getElementById("time_3").textContent = `${time} ms`;
+    document.getElementById("index_3").textContent = index.toString();
+    console.log("------------------------");
 }
+
+
+
+const consistent_search = element => {
+    arr.push(element + 1)
+    const start_time = performance.now();
+    let i = 0;
+    let ans;
+    let index = 0;
+    let current_el = arr[index];
+    while (i < count_iter) {
+        index = 0;
+        current_el = arr[index];
+        while (element > current_el) {
+            index++;
+            current_el = arr[index];
+        }
+        i++;
+    }
+    if (current_el === element) {
+        ans = [index, performance.now() - start_time];
+    } else {
+        ans = ["Не найден", performance.now() - start_time];
+    }
+
+    arr.pop();
+    return ans
+}
+
+
+const binary_search = element => {
+    arr.push(element + 1);
+    const start_time = performance.now();
+    let i = 0;
+    let ans;
+    let L = 0;
+    let R = arr.length;
+    let current_el;
+    let current_ind;
+    while (i < count_iter) {
+        L = 0;
+        R = arr.length;
+        current_el = null;
+        current_ind = null;
+
+        while (L <= R) {
+            current_ind = Math.floor((L + R) / 2);
+            current_el = arr[current_ind];
+            if (current_el == element) {
+                break
+            } else if (element < current_el) {
+                R = current_ind - 1
+            } else {
+                L = current_ind + 1
+            }
+        }
+        i++;
+
+    }
+    if (current_el === element) {
+        ans = [current_ind, performance.now() - start_time];
+    } else {
+        ans = ["Не найден", performance.now() - start_time];
+    }
+    arr.pop();
+    return ans
+
+}
+
+const opt_binary_search = element => {
+    arr.push(element + 1);
+    const start_time = performance.now();
+    let i = 0;
+    let ans;
+    let L = 0;
+    let R = arr.length;
+    let current_el;
+    let current_ind;
+    while (i < count_iter) {
+        L = 0;
+        R = arr.length;
+        current_el = null;
+        current_ind = null;
+
+        while (L < R) {
+            current_ind = Math.floor((L + R) / 2);
+            current_el = arr[current_ind];
+            if (element <= current_el) {
+                R = current_ind
+            } else {
+                L = current_ind + 1
+            }
+        }
+        i++;
+    }
+    current_ind = Math.floor((L + R) / 2);
+    current_el = arr[current_ind];
+    if (current_el === element) {
+        ans = [current_ind, performance.now() - start_time];
+    } else {
+        ans = ["Не найден", performance.now() - start_time];
+    }
+    arr.pop();
+    return ans
+}
+
+console.log(arr);
+console.log("parseInt(\"000003\") is", parseInt("000003"));
