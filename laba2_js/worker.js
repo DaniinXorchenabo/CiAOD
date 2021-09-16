a64c2fcb61f6e8b0e5a91aae0efd0a23557c3a89
DaniinXorchenabo <rkbcu@mail.ru>
2021-09-16 12:52:00 +0300

    обавлено два вида интерполяционного бинарного поиска

--- laba2_js/worker.js
+++ laba2_js/worker.js
@@ -1,12 +1,13 @@
 let arr = [];
 setTimeout(() => {
     let i = 0;
-    while (i < 10e5){
-        arr.push(Math.random().toString().substr(3, 5));
+    while (i <  10e5){
+        // arr.push(Math.random().toString().substr(3, 7));
+        arr.push(i)
         i++;
     }
-    arr.sort()
-    arr = arr.map(i => parseInt(i));
+    // arr.sort()
+    // arr = arr.map(i => parseInt(i));
     console.log(arr);
 })
 const count_iter = 10000;
@@ -39,7 +40,24 @@ const processing = id_element => {
 
     document.getElementById("time_3").textContent = `${time} ms`;
     document.getElementById("index_3").textContent = index.toString();
+
+    console.time('time_4');
+    [index, time] = inter_binary_search(key);
+    console.timeEnd('time_4');
+    time = Math.round(time);
+
+    document.getElementById("time_4").textContent = `${time} ms`;
+    document.getElementById("index_4").textContent = index.toString();
+
+    console.time('time_5');
+    [index, time] = opt_inter_binary_search(key);
+    console.timeEnd('time_5');
+    time = Math.round(time);
+
+    document.getElementById("time_5").textContent = `${time} ms`;
+    document.getElementById("index_5").textContent = index.toString();
     console.log("------------------------");
+
 }
 
 
@@ -77,12 +95,12 @@ const binary_search = element => {
     let i = 0;
     let ans;
     let L = 0;
-    let R = arr.length;
+    let R = arr.length -1;
     let current_el;
     let current_ind;
     while (i < count_iter) {
         L = 0;
-        R = arr.length;
+        R = arr.length - 1;
         current_el = null;
         current_ind = null;
 
@@ -110,18 +128,19 @@ const binary_search = element => {
 
 }
 
+
 const opt_binary_search = element => {
     arr.push(element + 1);
     const start_time = performance.now();
     let i = 0;
     let ans;
     let L = 0;
-    let R = arr.length;
+    let R = arr.length - 1;
     let current_el;
     let current_ind;
     while (i < count_iter) {
         L = 0;
-        R = arr.length;
+        R = arr.length - 1;
         current_el = null;
         current_ind = null;
 
@@ -136,9 +155,103 @@ const opt_binary_search = element => {
         }
         i++;
     }
-    current_ind = Math.floor((L + R) / 2);
-    current_el = arr[current_ind];
+    // current_ind = Math.floor((L + R) / 2);
+    // current_el = arr[current_ind];
+    if (arr[R] === element) {
+        ans = [current_ind, performance.now() - start_time];
+    } else {
+        ans = ["Не найден", performance.now() - start_time];
+    }
+    arr.pop();
+    return ans
+}
+
+
+const inter_binary_search = element => {
+    arr.push(element + 1);
+    const start_time = performance.now();
+    let i = 0;
+    let ans;
+    let L = 0;
+    let R = arr.length -1;
+    let current_el;
+    let current_ind;
+    while (i < count_iter) {
+        L = 0;
+        R = arr.length -1;
+        current_el = null;
+        current_ind = null;
+
+        while (L <= R) {
+            current_ind = Math.round(L + (element - arr[L]) * (R - L) / (arr[R] - arr[L]));
+            current_el = arr[current_ind];
+            if (current_el == element) {
+                break
+            } else if (element < current_el) {
+                R = current_ind - 1
+            } else {
+                L = current_ind + 1
+            }
+        }
+        i++;
+
+    }
     if (current_el === element) {
+        ans = [current_ind, performance.now() - start_time];
+    } else {
+        ans = ["Не найден", performance.now() - start_time];
+    }
+    arr.pop();
+    return ans
+
+}
+
+
+const opt_inter_binary_search = element => {
+    arr.push(element + 1);
+    const start_time = performance.now();
+    let i = 0;
+    let ans;
+    let L = 0;
+    let R = arr.length -1;
+    let current_el;
+    let current_ind;
+    while (i < count_iter) {
+        L = 0;
+        R = arr.length - 1;
+        current_el =  null;
+        current_ind = null;
+        let k = null
+        while (L < R) {
+
+            current_ind = Math.round(L + (R - L) * (element - arr[L]) / (arr[R] - arr[L]));
+            current_el = arr[current_ind];
+
+            if (element <= current_el) {
+                R = current_ind;
+            } else {
+                L = current_ind + 1;
+            }
+
+            if (L >= R){
+                break
+            }
+
+            current_ind = Math.round(L + (R - L) * (element - arr[L]) / (arr[R] - arr[L]));
+            current_el = arr[current_ind];
+
+            if (element < current_el) {
+                R = current_ind - 1;
+            } else {
+                L = current_ind;
+            }
+
+        }
+        i++;
+    }
+    // current_ind = Math.round(L + ((element - arr[L]) * (R - L) / (arr[R] - arr[L])));
+    // current_el = arr[current_ind];
+    if (arr[R] === element) {
         ans = [current_ind, performance.now() - start_time];
     } else {
         ans = ["Не найден", performance.now() - start_time];
